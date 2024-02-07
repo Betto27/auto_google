7cea07422829907e7ea31bf475b563e69f0c97fc
Betto <betto_beto@hotmail.com>
2024-02-07 12:38:10 -0300

    Initial commit

---
+++ browser.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from time import sleep
+
+class Browser(object):
+    # Inicia o browser chrome, mas pode ser feito com outros como Firefox, Safari e IE
+    driver = webdriver.Chrome()
+    # Define o tempo máximo para carregamento da página
+    driver.set_page_load_timeout(30)
+    # Maximiza a janela do browser ao ser iniciado
+    driver.maximize_window()
+
+    # Fecha o browser
+    def browser_quit(self):
+        sleep(5)
+        self.driver.quit()
+
+    # Limpa o browser
+    def browser_clear(self):
+        self.driver.delete_all_cookies()
+        self.driver.execute_script('window.localStorage.clear()')
+        self.driver.execute_script('window.sessionStorage.clear()')
+        self.driver.refresh()
